e1dbd0daa52cbd6ac1273f4e27bbf97ccf56c8c5
Taiwan marinho <clownser1994@gmail.com>
2020-11-23 13:36:54 -0300

    Exercicio n:1

---
+++ Exercicio/Exercicio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float divisor = 0;
+            float dividendo =0;
+            float resultado = 0;
+
+         for(;;){
+            Console.WriteLine("Informe o dividendo: ");
+            dividendo =float.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o divisor: ");
+            divisor =float.Parse(Console.ReadLine());
+
+            if(divisor == 0){
+                Console.WriteLine("Divisão impossivel!");
+                Console.WriteLine();
+                
+            }
+            else{
+                resultado = dividendo/divisor;
+                Console.WriteLine();
+                Console.WriteLine($"O resultado da divisão e: {resultado}");
+                break;
+
+            }
+        }
+
+        }
+    }
+}
